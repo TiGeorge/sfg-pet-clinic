cac6ea6e88e374ea6521d4af4e6b623edff52f11
George Tikhomirov <georgetikhomirov@mail.ru>
2020-04-02 20:18:20 +0300

    Closes #27

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,8 +1,16 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
-    public static void main(String[] args) {
 
+    private Set<Speciality> specialitys;
 
+    public Set<Speciality> getSpecialitys() {
+        return specialitys;
+    }
+
+    public void setSpecialitys(Set<Speciality> specialitys) {
+        this.specialitys = specialitys;
     }
 }
