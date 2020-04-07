14ef8ada0489d4cc37f2d98c4f1ff764e7cf3be0
TiGeorge <georgetikhomirov@mail.ru>
2020-04-07 16:06:59 +0300

    Closes #51

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,9 +1,16 @@
 package guru.springframework.sfgpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
@@ -13,11 +20,4 @@ public class Vet extends Person {
             inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialitys = new HashSet<>();
 
-    public Set<Speciality> getSpecialitys() {
-        return specialitys;
-    }
-
-    public void setSpecialitys(Set<Speciality> specialitys) {
-        this.specialitys = specialitys;
-    }
 }
